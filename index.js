6e3ece67578d4c5b42080428cc7680d0ea425c7f
Aurelio Gonzalez <agg156231@gmail.com>
2021-01-18 06:40:41 -0600

    manejo de errores1 al metodo newTable y se creo el metodo newBD

---
+++ index.js
@@ -0,0 +1,4 @@
+const bd=require('./process/bd_json');
+bd.newBD("regros");
+bd.statusBD="regros";
+bd.newTable("usuarios-nel")
--- process/bd_json.js
+++ process/bd_json.js
@@ -1,10 +1,27 @@
 const fs = require("fs");
 const process = require("process");
 //cat > ${a}.json
+let statusBD=null;
+
 function newBD(namebd){
-  fs.writeFile(`./${namebd}.json`, "[]", function (error) {
-    if (error) console.log(error);
-    else console.log("El archivo fue creado");
-  });
+  fs.mkdirSync(`./data/databases/${namebd}`);
+}
+function newTable(nametable){
+  if(statusBD===null){
+    console.log("BD not selected");
+  }else if(fs.existsSync(`./data/databases/${statusBD}/${nametable}.json`)){
+    console.log(`BD: ${statusBD}\n${nametable}.json already exists`);
+  }else{
+    fs.writeFile(`./data/databases/${statusBD}/${nametable}.json`, "[]", function (error) {
+      if (error) console.log(error);
+      else console.log(`BD: ${statusBD}\nTable created: ${nametable}`);
+    });
+  }
+  
+}
+module.exports={
+  newBD,
+  newTable,
+  statusBD
 }
 
