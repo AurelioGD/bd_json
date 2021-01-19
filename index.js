44c953e7ffa19965912dde05431eb71f53cda968
Aurelio Gonzalez <agg156231@gmail.com>
2021-01-19 01:47:27 -0600

    Se depuro el metodo newTable

--- index.js
+++ index.js
@@ -3,4 +3,3 @@ const fs=require('fs');
 /* bd.newBD("regros");
 bd.statusBD="regros";
 bd.newTable("usuarios-nel") */
-console.log(fs.existsSync(`./data/databases/conchitaBD`));
--- process/bd_json.js
+++ process/bd_json.js
@@ -6,30 +6,39 @@ function getStatus() {
 }
 
 function setStatus(namestatus) {
-  const statusA = getStatus();
-  statusA.status = namestatus;
-  fs.writeFileSync("./process/settings.json", JSON.stringify(statusA));
+  const statusBD = getStatus();
+  statusBD.status = namestatus;
+  fs.writeFileSync("./process/settings.json", JSON.stringify(statusBD));
 }
 
 function newBD(namebd) {
   fs.mkdirSync(`./data/databases/${namebd}`);
 }
 
-function newTable(nametable) {
-  if (statusBD === null) {
-    console.log("BD not selected");
-  } else if (fs.existsSync(`./data/databases/${statusBD}/${nametable}.json`)) {
-    console.log(`BD: ${statusBD}\n${nametable}.json already exists`);
-  } else {
-    fs.writeFile(
-      `./data/databases/${statusBD}/${nametable}.json`,
-      "[]",
-      function (error) {
-        if (error) console.log(error);
-        else console.log(`BD: ${statusBD}\nTable created: ${nametable}`);
-      }
-    );
+function newTable(name) {
+  let statusBD=getStatus().status;
+  if(typeof(name)!=='string'){
+    console.log("ERROR BD: invalid data type.");
+  }else{
+    let nametable=name.trim().replace(/ /g, "_");
+    if(nametable===""){
+      console.log("ERROR BD: Invalid name")
+    }else if (!fs.existsSync(`./data/databases/${statusBD}`)) {
+      console.log("ERROR BD: not selected or not exist.");
+    } else if (fs.existsSync(`./data/databases/${statusBD}/${nametable}.json`)) {
+      console.log(`ERROR BD: ${statusBD}\n${nametable}.json already exists.`);
+    } else {
+      fs.writeFile(
+        `./data/databases/${statusBD}/${nametable}.json`,
+        "[]",
+        function (error) {
+          if (error) console.log(error);
+          else console.log(`BD message: ${statusBD}\nTable created: ${nametable}`);
+        }
+      );
+    }
   }
+  
 }
 module.exports = {
   newBD,
