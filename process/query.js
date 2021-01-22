62057ff9fdb60052988c2a57baa1be7ee41506b7
Aurelio Gonzalez <agg156231@gmail.com>
2021-01-22 05:00:18 -0600

    Se agregaron unas correcciones, Ya se esta preparado para empezar a reaÃlizar la ejecucion de la sentencia select

--- process/query.js
+++ process/query.js
@@ -2,17 +2,24 @@ const fs = require("fs");
 const va = require("./validations");
 
 function Select(table, condition, ...columns) {
-  if (va.vaParam(table) && va.vaParam(condition)&&va.vaArrayString(columns)) {
-    let newVaCondition = va.vaCondition(condition);
-    if (newVaCondition) {
-      console.log("la condicion esta al 100");
-      console.log(newVaCondition);
-    } else {
-      console.log(
-        "ERROR SELECT: Format of the incorrect condition, Valid format: (Column to validate)(Space)(Relational operator)(Space)(Value to compare)"
-      );
+    if(condition){
+        if (va.vaParam(table) && va.vaParam(condition)&&va.vaArrayString(columns)) {
+            let newVaCondition = va.vaCondition(condition);
+            if (newVaCondition) {
+              console.log("la condicion esta al 100");
+              console.log(newVaCondition);
+            } else {
+              console.log(
+                "ERROR SELECT: Format of the incorrect condition, Valid format: (Column to validate)(Space)(Relational operator)(Space)(Value to compare)"
+              );
+            }
+          }
+    }else{
+        if (va.vaParam(table)&&va.vaArrayString(columns)) {
+                console.log("todo al 100 pero no hay condicion");
+          }
     }
-  }
+  
 }
 
 module.exports = {
